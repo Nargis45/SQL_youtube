eb11da14b47edadddef99ea4312a9b7b254dddba
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2023-01-22 10:42:09 +0530

    Add files via upload

---
+++ sql_select_statement.sql
@@ -0,0 +1,18 @@
+use employees;
+
+SELECT 
+    *
+FROM
+    employees;
+    
+SELECT 
+    first_name, last_name
+FROM
+    employees;
+    
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    first_name = 'Georgi';
