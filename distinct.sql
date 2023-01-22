88319a4d213421708ab5eb4e663c7cc237807048
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2023-01-22 23:45:36 +0530

    Add files via upload

---
+++ distinct.sql
@@ -0,0 +1,19 @@
+SELECT 
+    *
+FROM
+    employees;
+
+SELECT 
+    hire_date
+FROM
+    employees;
+
+SELECT DISTINCT
+    YEAR(hire_date)
+FROM
+    employees;
+    
+SELECT DISTINCT
+    gender
+FROM
+    employees;
