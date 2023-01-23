d15ec85e032ab23bda3fdbc8d8124de37ee5fe0f
Nargis Nasreen <60965420+Nargis45@users.noreply.github.com>
2023-01-23 11:04:50 +0530

    Add files via upload

---
+++ order_by.sql
@@ -0,0 +1,17 @@
+use employees;
+SELECT 
+    *
+FROM
+    employees;
+    
+SELECT 
+    *
+FROM
+    employees
+ORDER BY first_name, last_name;
+
+SELECT 
+    *
+FROM
+    employees
+ORDER BY emp_no;
